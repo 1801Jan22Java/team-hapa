a06736a7b812be320747ea6ea6c8afb5cc6ab362
Kirik0 <mkkim6@asu.edu>
2018-02-28 00:29:11 -0500

    Updated FeedbackDao with the additional method

--- MACSAirport/src/main/java/com/revature/dao/FeedbackDao.java
+++ MACSAirport/src/main/java/com/revature/dao/FeedbackDao.java
@@ -5,5 +5,5 @@ import com.revature.domain.Feedback;
 public interface FeedbackDao {
 	public Feedback getFeedbackById(int id);
 	public int addFeedback(Feedback thisFeedback);
-	
+	public List<Feedback> getAllFeedback();
 }

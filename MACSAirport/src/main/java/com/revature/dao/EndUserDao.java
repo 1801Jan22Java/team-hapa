ee1e31e30e0fdba130e10924f44b6abb5d2f9cb5
Kirik0 <mkkim6@asu.edu>
2018-02-28 00:30:22 -0500

    Added getAllEndUsers and updateEndUser

--- MACSAirport/src/main/java/com/revature/dao/EndUserDao.java
+++ MACSAirport/src/main/java/com/revature/dao/EndUserDao.java
@@ -5,5 +5,8 @@ import com.revature.domain.EndUser;
 public interface EndUserDao {
 	public EndUser getEndUserById(int id);
 	public EndUser getEndUserByEmail(String email);
+	public List<EndUser> getAllEndUsers();
 	public int addEndUser(EndUser thisEndUser);
+	public void updateEndUser(EndUser u);
+	
 }

4d43916a937b50e7efb8c966586ec7c04f2834cc
Ahmed Awwad <sherman6@cs.washington.edu>
2018-03-07 05:38:20 -0500

    added logging features on login, register, error

---
+++ MACSAirport/src/main/java/com/revature/aspect/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.revature.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component(value="loggingAspect")
+public class LoggingAspect {
+	
+	private static Logger log = Logger.getRootLogger();
+	
+	@AfterThrowing("within(com.revature.*.*)")
+	public void logAfterThrow(JoinPoint jp){
+		log.error(jp.getSignature());
+	}
+	
+}
